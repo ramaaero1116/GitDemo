4020d01c55c69b713286c8df03a8c3c25a99a4d0
ram <ramaaero1116@gmail.com>
2019-04-19 13:02:06 +0100

    first commit from gitX

--- src/test/java/qaclickacademy/Mavenjava2/RestAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava2/RestAPITest.java
@@ -10,7 +10,9 @@ public void postJira(){
 	System.out.println("postJira2");
 	System.out.println("postJira3");
 	System.out.println("postJira4");
-	
+	//
+	System.out.println("postJira5");
+	System.out.println("postJira6");	
 	
 }
 @Test
